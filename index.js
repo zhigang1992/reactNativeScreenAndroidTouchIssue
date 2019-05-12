ec55144e778677d4f94fc7baf148b42a4ff7378c
Kyle Fang <zhigang1992@gmail.com>
2019-05-12 09:55:42 +0800

    chore: add screesn

--- index.js
+++ index.js
@@ -5,5 +5,8 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import { useScreens } from 'react-native-screens';
+
+useScreens();
 
 AppRegistry.registerComponent(appName, () => App);
